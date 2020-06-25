f226130790d749e8e2f544e43e6013e9f7efef24
sarival <sarival@sarival.com.br>
2020-06-24 22:32:14 -0300

    Criando teste LanceArgumentExceptionDadoValorNegativo

---
+++ Alura.LeilaoOnline.Tests/LanceCtor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Alura.LeilaoOnline.Core;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LanceCtor
+    {
+        [Fact]
+        public void LanceArgumentExceptionDadoValorNegativo()
+        {
+            var ex = Assert.Throws<ArgumentException>(
+                () => new Lance(null, -100)
+            );           
+        }
+    }
+}
